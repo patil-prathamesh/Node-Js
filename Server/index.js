dd86a218f4dbe12f2476a65c991bc41b24c97c02
prathamesh patil <patilprathamesh696@gmail.com>
2023-08-05 17:18:49 +0530

    modified

--- Server/index.js
+++ Server/index.js
@@ -4,4 +4,4 @@ const myserver = http.createServer((req,res) => {
     console.log("Welcome to my server...");
     res.end("Hello from server..");
 })
-myserver.listen(8000, ()=>console.log("server started..."))
+myserver.listen(8000, ()=>console.log("server started..."));
